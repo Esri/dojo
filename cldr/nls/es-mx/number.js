1d88557d1285cdd4be51809b6a027e7d5bbfefa2
Ganesh Subbiah <gsubbiah@esri.com>
2019-02-19 13:03:52 -0600

    The XML based CLDR spec for `es-MX` didnt list the decimal, group and other number related symbols which was listed in the CLDR Json spec, so ported that to dojo/cldr for `es-mx` (WebGIS/arcgis-portal-app#22145)

--- cldr/nls/es-mx/number.js
+++ cldr/nls/es-mx/number.js
@@ -1,6 +1,18 @@
 define(
 //begin v1.x content
 {
+  "decimal": ".",
+  "group": ",",
+  "list": ";",
+  "percentSign": "%",
+  "plusSign": "+",
+  "minusSign": "-",
+  "exponential": "E",
+  "superscriptingExponent": "×",
+  "perMille": "‰",
+  "infinity": "∞",
+  "nan": "NaN",
+  "timeSeparator": ":",
 	"percentFormat": "#,##0%"
 }
 //end v1.x content
