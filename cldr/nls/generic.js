b1602e9a4cd056474972e50bc44dd5cf740e70e4
Ganesh Subbiah <gsubbiah@esri.com>
2020-11-09 10:56:23 -0600

    feat: added Slovak CLDR files

--- cldr/nls/generic.js
+++ cldr/nls/generic.js
@@ -771,6 +771,7 @@ define({ root:
 	"pt-pt": true,
 	"ro": true,
 	"ru": true,
+	"sk": true,
 	"sl": true,
 	"sr": true,
 	"sv": true,
---
+++ cldr/nls/sk/number.js
@@ -0,0 +1,25 @@
+define(
+//begin v1.x content
+{
+	"decimal": ",",
+	"group": " ",
+	"list": ";",
+	"percentSign": "%",
+	"plusSign": "+",
+	"minusSign": "-",
+	"exponential": "e",
+	"superscriptingExponent": "×",
+	"perMille": "‰",
+	"infinity": "∞",
+	"nan": "NaN",
+	"timeSeparator": ":",
+	"decimalFormat": "#,##0.###",
+	"decimalFormat-long": "000 biliónov",
+	"decimalFormat-short": "000 bil'.'",
+	"scientificFormat": "#E0",
+	"percentFormat": "#,##0 %",
+	"currencyFormat": "#,##0.00 ¤;(#,##0.00 ¤)",
+	"currencyFormat-short": "000 bil'.' ¤"
+}
+//end v1.x content
+);
